c811e665e1c692734322f8e61a52bb7ae274ad42
DanielDiaz824 <daniel.diazmijares.148@gmail.com>
2019-11-12 09:46:39 -0600

    kk

--- reservaciones.php
+++ reservaciones.php
@@ -3,11 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Reservaciones</title>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/all.min.css"/>
+    <link rel="stylesheet" href="css/main.css">
+    
 
     <style>
     .salon{
         margin:40px;
+    }
+    .silla-reservada{
+        color:red;
     }
         .contenedor-mesa{
             margin:5px;
@@ -61,7 +69,7 @@
     <section class="salon">
         <div class="contenedor-mesa" data-mesa="1">
             <i class="fas fa-dot-circle mesa"></i>
-            <i class="fas fa-chair silla silla-pos1"></i>
+            <i class="fas fa-chair silla silla-pos1 silla-reservada" title="Silla ya reservada :(" data-toggle="tooltip" data-placement="right"></i>
             <i class="fas fa-chair silla silla-pos2"></i>
             <i class="fas fa-chair silla silla-pos3"></i>
             <i class="fas fa-chair silla silla-pos4"></i>
@@ -81,28 +89,12 @@
             <i class="fas fa-chair silla silla-pos7"></i>
             <i class="fas fa-chair silla silla-pos8"></i>
         </div>
-        <div class="contenedor-mesa" data-mesa="3">
-            <i class="fas fa-dot-circle mesa"></i>
-            <i class="fas fa-chair silla silla-pos1"></i>
-            <i class="fas fa-chair silla silla-pos2"></i>
-            <i class="fas fa-chair silla silla-pos3"></i>
-            <i class="fas fa-chair silla silla-pos4"></i>
-            <i class="fas fa-chair silla silla-pos5"></i>
-            <i class="fas fa-chair silla silla-pos6"></i>
-            <i class="fas fa-chair silla silla-pos7"></i>
-            <i class="fas fa-chair silla silla-pos8"></i>
-        </div>
-        <div class="contenedor-mesa" data-mesa="4">
-            <i class="fas fa-dot-circle mesa"></i>
-            <i class="fas fa-chair silla silla-pos1"></i>
-            <i class="fas fa-chair silla silla-pos2"></i>
-            <i class="fas fa-chair silla silla-pos3"></i>
-            <i class="fas fa-chair silla silla-pos4"></i>
-            <i class="fas fa-chair silla silla-pos5"></i>
-            <i class="fas fa-chair silla silla-pos6"></i>
-            <i class="fas fa-chair silla silla-pos7"></i>
-            <i class="fas fa-chair silla silla-pos8"></i>
-        </div>
+        
     </section>
+    <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        });
+    </script>
 </body>
 </html>
