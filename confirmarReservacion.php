183bcdf438004e3c8f7530db4463918f7ff6c7c4
DanielDiaz824 <daniel.diazmijares.148@gmail.com>
2019-11-26 09:45:19 -0600

    ko

--- confirmarReservacion.php
+++ confirmarReservacion.php
@@ -3,10 +3,23 @@
     $idUsuario=$_SESSION["datosUsuario"]["id"];
     include("conexion.php");
     $idSilla=$_POST["silla"];
-    $paquete=$_POST["paquete"];
+
+    $sql = "SELECT * FROM usuarios_paquetes";
+    $paquete=$conexionBD->query($sql);
+    if(mysqli_num_rows($paquete)>0){
+        $fila = $paquete->fetch_array(MYSQLI_ASSOC);
+
+        $_SESSION['Usuario']=$fila['idUsuario'];
+        $_SESSION['Paquete']=$fila['paquete'];
+        $_SESSION['Lugares']=$fila['lugares'];
+    }
+    else {
+        echo "No hay resultados";
+    }
+    $paquete1=$_SESSION['Paquete'];
 
 
-    $statementr="INSERT INTO reservaciones (id_silla,id_usuario,paquete) VALUES ('$idSilla','$idUsuario','$paquete')";  
+    $statementr="INSERT INTO reservaciones (id_silla,id_usuario,paquete) VALUES ('$idSilla','$idUsuario','$paquete1')";  
     
     $resultado= $conexionBD->query($statementr);
     if($resultado){
